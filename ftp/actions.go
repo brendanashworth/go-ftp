4b44c3299e943bc6bced209f6dee380c54dc56cd
boboman13 <rich@safe-node.com>
2014-04-21 16:20:03 -0400

    Move files for library

---
+++ ftp/actions.go
@@ -0,0 +1,70 @@
+package ftp
+
+import (
+	"fmt"
+	"strings"
+)
+
+// Client QUIT
+func (this *FTPClient) QUIT() {
+	this.SendMessage(221)
+	this.Close()
+}
+
+// Client USER
+func (this *FTPClient) USER(message string) {
+	// grab username
+	username := strings.Trim(message, " ")
+	this.user = username
+
+	this.SendMessage(331)
+}
+
+// Client PASS
+func (this *FTPClient) PASS(message string) {
+	// check if there is a username
+	if this.user == "" {
+		this.SendMessage(503)
+		return
+	}
+
+	// check if not authentication
+	if this.authenticated {
+		this.SendMessage(503)
+		return
+	}
+
+	// grab password
+	password := message
+	this.password = password
+
+	// authenticate
+	authenticated, dir := this.server.config.authenticationCheck(this.user, this.password)
+	this.authenticated = authenticated
+	this.dir = dir
+	
+	if this.authenticated {
+		this.SendMessage(230)
+	} else {
+		this.SendMessage(230)
+	}
+}
+
+// Client SYST
+func (this *FTPClient) SYST() {
+	this.SendMessage(215)
+}
+
+// Client FEAT
+func (this *FTPClient) FEAT() {
+	this.SendMessage(502)
+}
+
+func (this *FTPClient) CWD(message string) {
+	fmt.Println("Changed working directory to: " + message)
+}
+
+// Client, command not implemented.
+func (this *FTPClient) NOTIMP() {
+	this.SendMessage(502)
+}
---
+++ ftp/constants.go
@@ -0,0 +1,5 @@
+package ftp
+
+const (
+	RCV_BUFFER_LENGTH = 1024
+)
