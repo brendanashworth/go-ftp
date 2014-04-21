85e67ec63186348f1c9dbe167535b0191ba6ceaf
boboman13 <rich@safe-node.com>
2014-04-21 16:27:17 -0400

    Still working out the kinks

--- example/server.go
+++ example/server.go
@@ -2,15 +2,24 @@ package main
 
 import (
 	"fmt"
-	"github.com/boboman13/go-ftp"
+	"github.com/boboman13/go-ftp/ftp"
 )
 
+// Starts server
 func main() {
-	server := &FTPServer{
-		host: "0.0.0.0",
-		port: "21",
+	server := &ftp.FTPServer{
+		Host: "0.0.0.0",
+		Port: 21,
+		Config: new(ftp.AuthenticationConfig),
 	}
 
+	// Configures authentication; WARNING: do not use this code, it is insecure
+	server.config.ConfigAuthentication(func(user string, password string) (authenticated bool, dir string) {
+		fmt.Println("Logged in " + user + " w/ pass " + password)
+		return true, "/home/" + user + "/ftp"
+		})
+
+	// Starts server
 	err := server.Start()
 	if err != nil {
 		fmt.Println("Error occurred starting FTP server: " + err.Error())
