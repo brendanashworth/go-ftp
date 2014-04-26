d8f173f1f4e2f12dc3752d69f40ab520a26b4539
boboman13 <rich@safe-node.com>
2014-04-26 01:59:57 -0400

    sup

--- utils/fileformat.go
+++ utils/fileformat.go
@@ -8,7 +8,7 @@ import (
 // Parses a file into the correct FTP-defined string.
 func ParseFile(file os.FileInfo, parseType string) string {
 	fileName := file.Name()
-	bytes := strconv.FormatUint(file.Size(), 10)
+	bytes := strconv.FormatInt(file.Size(), 10)
 
 	switch parseType {
 	// ASCII data format type
@@ -16,5 +16,7 @@ func ParseFile(file os.FileInfo, parseType string) string {
 
 		//      drwx------   3 slacker    users         104 Jul 27 01:45 public_html"   <- example
 		return "drwx------   3 slacker    users         " + bytes + " Jul 27 01:45 " + fileName
+	default:
+		return "HELLO THERE"
 	}
 }
